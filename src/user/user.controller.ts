fe833dbb163711d3632a6a4d5092d7450ee8373e
Yijyo <ljhmuse@gmail.com>
2024-09-16 19:36:25 +0900

    Commit2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    create(@Body() user: User): Promise<User> {
+        return this.userService.create(user);
+    }
+
+    @Get()
+    findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<User> {
+        return this.userService.findOne(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): Promise<void> {
+        return this.userService.remove(id);
+    }
+}
